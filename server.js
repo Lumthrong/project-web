caf5050866c30fd8489f06f97fdb04678465f7be
Lumthrong <iamrein22@gmail.com>
2025-05-31 18:44:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -62,6 +62,15 @@ const docUpload = multer({
     }
   }
 });
+//for email notifications 
+function escapeHtml(unsafe = '') {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
 
 const app = express();
 
